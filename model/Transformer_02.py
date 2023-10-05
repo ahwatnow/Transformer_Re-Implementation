2a8cb70bf138532d7caf118c526d1f1dc5d33308
ahwatnow <sohnji12@korea.ac.kr>
2023-10-05 20:11:58 +0900

    Relocated

--- model/Transformer_02.py
+++ model/Transformer_02.py
@@ -9,8 +9,22 @@ def get_device():
 
 def scaled_dot_product(q, k, v, mask=None):
     d_k = q.size()[-1]
+    scaled = torch.matmul(q, k.transpose(-1, -2)) / math.sqrt(d_k)
+    if mask is not None:
+        scaled = scaled.permute + mask
+    attention = F.softmax(scaled, dim=-1)
+    values = torch.matmul(attention, v)
+    return values, attention
 
-class PositionalEncoding():
+class PositionalEncoding(nn.Module):
+    
+    def __init__(self, d_model, max_sequence_length):
+            super().__init__()
+            self.max_sequence_length = max_sequence_length
+            self.d_model = d_model
+    
+    def forward(self):
+        
 
 class SentenceEmbedding():
 
