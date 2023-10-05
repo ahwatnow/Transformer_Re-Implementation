fa553adf97184d7ceb7d67deba09126622a93358
ahwatnow <sohnji12@korea.ac.kr>
2023-10-06 00:29:34 +0900

    transformer poitionwisefeedforward

--- model/Transformer_02.py
+++ model/Transformer_02.py
@@ -19,20 +19,116 @@ def scaled_dot_product(q, k, v, mask=None):
 class PositionalEncoding(nn.Module):
     
     def __init__(self, d_model, max_sequence_length):
-            super().__init__()
-            self.max_sequence_length = max_sequence_length
-            self.d_model = d_model
+        super().__init__()
+        self.max_sequence_length = max_sequence_length
+        self.d_model = d_model
     
     def forward(self):
-        
+        _2i = torch.arange(0, self.d_model, 2).float()
+        denominator = torch.pow(10000, _2i/self.d_model)
+        position = torch.arange(self.max_sequence_length).reshape(self.max_sequence_length, 1)
+        even_PE = torch.sin(position / denominator)
+        odd_PE = torch.cos(position / denominator)
+        stacked = torch.stack([even_PE, odd_PE], dim=2)
+        PE = torch.flatten(stacked, start_dim=1, end_dim=2)
+        return PE
 
-class SentenceEmbedding():
+
+class SentenceEmbedding(nn.Module):
+
+    def __init__(self, max_sequence_length, d_model, language_to_index, START_TOKEN, END_TOKEN, PADDING_TOKEN):
+        super().__init__()
+        self.vocab_size = len(language_to_index)
+        self.max_sequence_length = max_sequence_length
+        self.embedding = nn.Embedding(self.vocab_size, d_model)
+        self.language_to_index = language_to_index
+        self.position_encoder = PositionalEncoding(d_model, max_sequence_length)
+        self.dropout = nn.Dropout(p=0.1)
+        self.START_TOKEN = START_TOKEN 
+        self.END_TOKEN = END_TOKEN
+        self.PADDING_TOKEN = PADDING_TOKEN
+
+    def batch_tokenize(self, batch, start_token, end_token)
+        
+        def tokenize(sentence, start_token, end_token):
+            sentence_word_indices = [self.language_to_index[token] for token in list(sentence)]
+            if start_token:
+                sentence_word_indices.insert(0, self.language_to_index[self.START_TOKEN])
+            if end_token:
+                sentence_word_indices.append(self.language_to_index[self.END_TOKEN])
+            for _ in range(len(sentence_word_indices), self.max_sequence_length):
+                sentence_word_indicies.append(self.languages_to_index[self.PADDING_TOKEN])
+            return torch.tensor(sentence_word_indices)
+
+        tokenized=[]
+        for sentence_num in range(len(batch)):
+            tokenized.append( tokenize(batch[sentence_num], start_token, end_token))
+        tokenized = torch.stack(tokenized)
+        return tokenized.to(get_device())
+
+    def forward(self, x, start_token, end_token):
+        x = self.batch_tokenize(x, start_token, end_token)
+        x = self.embedding(x)
+        pos = self.position_encoder().to(get_device())
+        x = self.dropout(x + pos)
+        return x
 
 class MultiHeadAttention():
 
-class LayerNormalization():
+    def __init__(self, d_model, num_heads):
+        super().__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.head_dim = d_model // num_heads
+        self.qkv_layer = nn.Linear(d_model, 3 * d_model)
+        self.linear_layer = nn.Linear(d_model, d_model)
 
+    def forwad(self, x, mask):
+        batch_size, sequence_length, d_model = x.size()
+        qkv = self.qkv_layer(x)
+        qkv = qkv.reshape(batch_size, sequence_length, self.num_heads, 3 * self.head_dim)
+        qkv = qkv.permute(0, 2, 1, 3)
+        q, k, v = qkv.chunk(3, dim=-1)
+        values, attention = scaled_dot_product(q, k, v, mask)
+        values = values.permute(0, 2, 1, 3).reshape(batch_size, sequence_length, self.num_heads * self.head_dim)
+        out = self.linear_layer(values)
+        return out 
+
+
+class LayerNormalization(nn.Module):
+
+    def __init__(self, parameters_shape, eps=1e-5)
+        super().__init__()
+        self.parameters_shape = parameters_shape
+        self.eps = eps
+        self.gamma = nn.Parameter(torch.ones(parameters_shape))
+        self.beta = nn.Parameter(torch.zeros(parameters_shape))
+    
+    def forward(self, inputs):
+        dims = [-(i+1) for i in range(len(self.parameters_shape))]
+        mean = inputs.mean(dim = dims, keepdim = True)
+        var = ((inputs - mean) ** 2).mean(dim = dims, keepdim = True)
+        std = (var + self.eps).sqrt()
+        y = (inputs - mean) / std
+        out = self.gamma * y + self.beta 
+        return out 
+
+ 
 class PositionWiseFeedForward():
+    
+    def __init__(self, d_model, hidden, drop_prob = 0.1):
+        super(PositionWiseFeedForward, self).__init__()
+        self.linear1 = nn.Linear(d_model, hidden)
+        self.linear2 = nn.Linear(hidden, d_model)
+        self.relu == nn.ReLU()
+        self.dropout = nn.Dropout(p=drop_prob)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.linear2(x)
+        return x
 
 class EncoderLayer():
 
