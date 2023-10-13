0a9defa8c35e4536fdc2c8f3a5adf1f55414f5e8
ahwatnow <sohnji12@korea.ac.kr>
2023-10-13 23:45:43 +0900

    arg parsing done

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ parser.add_argument("--word_count", type=int, default=20000)
 parser.add_argument("--source", type=str, default = "en")
 parser.add_argument("--target", type=str, default="de")
 parser.add_argument("--config_dir", type=str, default="config.yaml"), 
-parser.add_argument()                    
+parser.add_argument("--num_gpus", type=int, default=2)
+
+args = parser.parse_args()
 
 # Load the IWSLT 2017 dataset for a specific language pair
 dataset = load_dataset("iwslt2017", "iwslt2017-en-de")
@@ -22,5 +24,14 @@ train_data = dataset["train"]
 valid_data = dataset["validation"]
 test_data = dataset["test"]
 
+if __name__ == "__main__":
 
+    logging.basicConfig(filename = "Transformer.log",
+                        level = logging.DEBUG,
+                        format = "%(Levelname)s %(asctime)s - %(message)s",
+                        filemode = 'w')
+    logger = logging.getLogger("TransformerLog")
+
+    logger.info("Our first message.")    
+    
 
