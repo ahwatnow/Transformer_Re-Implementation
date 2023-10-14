0c38ba58bb86cb72e083438c25048b7e7d5849c2
ahwatnow <sohnji12@korea.ac.kr>
2023-10-14 16:23:49 +0900

    config.yaml

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ parser.add_argument("--model_name", type=str, default="transformer")
 parser.add_argument("--word_count", type=int, default=20000)
 parser.add_argument("--source", type=str, default = "en")
 parser.add_argument("--target", type=str, default="de")
-parser.add_argument("--config_dir", type=str, default="config.yaml"), 
+parser.add_argument("--config_dir", type=str, default="config.yaml")
 parser.add_argument("--num_gpus", type=int, default=2)
 
 args = parser.parse_args()
